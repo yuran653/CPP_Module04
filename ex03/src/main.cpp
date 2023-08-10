998720ed89f6e8a985b9942d143b61b3cdd2bf5a
Iurii Ovsiannikov <jgoldste@z3t3c2.42bangkok.com>
2023-08-11 00:58:13 +0700

    ex03_is_started

---
+++ ex03/src/main.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/11 00:53:24 by jgoldste          #+#    #+#             */
+/*   Updated: 2023/08/11 00:57:32 by jgoldste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+int	main() {
+	IMateriaSource* src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+
+	ICharacter* me = new Character("me");
+	AMateria* tmp;
+
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+
+	ICharacter* bob = new Character("bob");
+
+	me->use(0, *bob);
+	me->use(1, *bob);
+
+	delete bob;
+	delete me;
+	delete src;
+
+	return 0;
+}
+
+// Output:
+// $> clang++ -W -Wall -Werror *.cpp
+// $> ./a.out | cat -e
+// * shoots an ice bolt at bob *$
+// * heals bob's wounds *$
